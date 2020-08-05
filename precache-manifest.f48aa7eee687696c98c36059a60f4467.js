48fe2649baf041e6ccfe379d7d5b7390198b9449
npatta01 <npatta01@gmail.com>
2020-08-05 13:33:07 +0000

    Deploying to gh-pages from  @ 3e181ab8c57c1efd904c5a51e4ebc687e862a92b 🚀

---
+++ precache-manifest.f48aa7eee687696c98c36059a60f4467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8236b585fa5587d7418bd9b4e81c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cdba6fc7e96170222f1",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "814135223e44c4a1da74",
+    "url": "/static/css/main.6666bb55.chunk.css"
+  },
+  {
+    "revision": "0cdba6fc7e96170222f1",
+    "url": "/static/js/2.b3fca743.chunk.js"
+  },
+  {
+    "revision": "e0e3057430b547fd93306d8129b1f682",
+    "url": "/static/js/2.b3fca743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814135223e44c4a1da74",
+    "url": "/static/js/main.dfe95513.chunk.js"
+  },
+  {
+    "revision": "ea38480b7a2a5f016251",
+    "url": "/static/js/runtime-main.43f2a34d.js"
+  }
+]);
